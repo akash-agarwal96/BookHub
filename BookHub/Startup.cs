05ca077476bcf336fb528f677067fb2b406a5ad8
akash-agarwal96 <akashbittu96@gmail.com>
2021-04-20 13:10:38 +0530

    Added product to cart and Session Implementation

--- BookHub/Startup.cs
+++ BookHub/Startup.cs
@@ -59,6 +59,12 @@ namespace BookHub
                 options.ClientSecret = "q_G3cHlko9UWilLhqnpsI2Qo";
 
             });
+            services.AddSession(options =>
+            {
+                options.IdleTimeout = TimeSpan.FromMinutes(30);
+                options.Cookie.HttpOnly = true;
+                options.Cookie.IsEssential = true;
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -79,6 +85,7 @@ namespace BookHub
             app.UseStaticFiles();
 
             app.UseRouting();
+            app.UseSession();
 
             app.UseAuthentication();
             app.UseAuthorization();
