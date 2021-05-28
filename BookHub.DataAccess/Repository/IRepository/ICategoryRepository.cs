1612c9d2140708d88e47f2f300d088c1d2ac5843
akash-agarwal96 <akashbittu96@gmail.com>
2021-05-28 23:19:07 +0530

    Added Async Pattern

--- BookHub.DataAccess/Repository/IRepository/ICategoryRepository.cs
+++ BookHub.DataAccess/Repository/IRepository/ICategoryRepository.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace BookHub.DataAccess.Repository.IRepository
 {
-    public interface ICategoryRepository : IRepository<Category>
+    public interface ICategoryRepository : IRepositoryAsync<Category>
     {
         void Update(Category category);
     }
--- BookHub.DataAccess/Repository/ShoppingCartRepository.cs
+++ BookHub.DataAccess/Repository/ShoppingCartRepository.cs
@@ -9,22 +9,18 @@ using System.Threading.Tasks;
 
 namespace BookHub.DataAccess.Repository
 {
-    public class CategoryRepository : Repository<Category>, ICategoryRepository
+    public class ShoppingCartRepository : Repository<ShoppingCart>, IShoppingCartRepository
     {
         private readonly ApplicationDbContext _db;
 
-        public CategoryRepository(ApplicationDbContext db) : base(db)
+        public ShoppingCartRepository(ApplicationDbContext db) : base(db)
         {
             _db = db;
         }
 
-        public void Update(Category category)
+        public void Update(ShoppingCart obj)
         {
-            var objFromDb = _db.Categories.FirstOrDefault(s => s.Id == category.Id);
-            if(objFromDb != null)
-            {
-                objFromDb.Name = category.Name;
-            }
+            _db.Update(obj);
         }
     }
 }
